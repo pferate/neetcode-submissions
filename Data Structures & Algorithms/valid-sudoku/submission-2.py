5ad70fc7e74d013d51c509ada3cdb32ea73f956a
Pat Ferate <pferate@users.noreply.github.com>
2026-06-16 13:44:56 -0700

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,28 @@
+import itertools
+
+class Solution:
+    
+    def isAllUnique(self, nums: List[str]) -> bool:
+        filtered_nums = [x for x in nums if x != "."]
+        return len(filtered_nums) == len(set(filtered_nums))
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Check Rows
+        for row in board:
+            if not self.isAllUnique(row):
+                return False
+
+        # Check Columns
+        for y in range(9):
+            if not self.isAllUnique([row[y] for row in board]):
+                return False
+
+        # Check Squares
+        for y_offset in range(0, 9, 3):
+            for x_offset in range(0, 9, 3):
+                nums = []
+                for y in range(3):
+                    nums += board[y_offset + y][x_offset:x_offset + 3]
+                if not self.isAllUnique(nums):
+                    return False
+        return True
