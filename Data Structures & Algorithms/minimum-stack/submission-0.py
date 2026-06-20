7272b21c13be3dd11df0ddde38a9f6a065867e7c
Pat Ferate <pferate@users.noreply.github.com>
2026-06-20 00:32:56 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+
+class MinStack:
+
+    def __init__(self):
+        self.items = deque()
+
+    def push(self, val: int) -> None:
+        self.items.append(val)
+        
+    def pop(self) -> None:
+        self.items.pop()
+        
+    def top(self) -> int:
+        return self.items[-1]
+        
+    def getMin(self) -> int:
+        return min(self.items)       
